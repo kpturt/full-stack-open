5ec09a73263222b6b4c1a78c9422a81c2827ec40
kpturt <kp.turtiainen7@gmail.com>
2021-08-02 18:47:28 +0300

    did 1.10

--- exercises/part1/exercise-unicafe/src/App.js
+++ exercises/part1/exercise-unicafe/src/App.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  // tallenna napit omaan tilaansa
+  
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   let all = good + neutral + bad
-  let average = 0
 
   const addGood = () => {
     console.log('added one good review')
@@ -23,45 +22,29 @@ const App = () => {
     setBad(bad +1)
   }
 
-  const handleClick = () => {
-    console.log('click')
-  }
-
-  //why doesn't work outside of the function and is one click behind?
-  const calculateAverage = () => {
-    console.log('calcucalting average')
-    all = all +1
-    console.log('all: ',all)
-    average = (good-bad)/(good+bad+neutral)
-    console.log('average: ',average)
-  }
-
   return (
     <div>
       <h1>give feedback</h1>
-
       <div>
-        <button onClick={() => { handleClick(); addGood(); calculateAverage(); }}>
-          good
-        </button>
-        <button onClick={() => {handleClick(); addNeutral(); calculateAverage(); }}>
-          neutral
-        </button>
-        <button onClick={() => {handleClick(); addBad(); calculateAverage(); }}>
-          bad
-        </button>
+        <Button handleClick={addGood} text="good" />
+        <Button handleClick={addNeutral} text="neutral" />
+        <Button handleClick={addBad} text="bad" />
       </div>
-
       <h1>statistics</h1>
-
-      <Statistics good={good} neutral={neutral} bad={bad} all={all}/>
-      
+      <Statistics good={good} neutral={neutral} bad={bad} all={all}/> 
     </div>
   )
 }
 
-const Statistics = (props) => {
+const Button = (props) => {
+  return (
+      <button onClick={props.handleClick} > 
+        {props.text} 
+      </button>
+  )
+}
 
+const Statistics = (props) => {
   if(props.good+props.neutral+props.bad === 0){
     console.log('No feedback given.')
     return (
@@ -74,15 +57,23 @@ const Statistics = (props) => {
     console.log('Feedback has been given.')
     return (
       <div>
-        <div>good {props.good}</div>
-        <div>neutral {props.neutral}</div>
-        <div>bad {props.bad}</div>
-        <div>all {props.all}</div>
-        <div>average {(props.good-props.bad)/(props.good+props.bad+props.neutral)}</div>
-        <div>positive {(100*props.good)/(props.good+props.bad+props.neutral)} %</div>
+        <StatisticLine text="good" value={props.good}/>
+        <StatisticLine text="neutral" value={props.neutral}/>
+        <StatisticLine text="bad" value={props.bad}/>
+        <StatisticLine text="all" value={props.all} />
+        <StatisticLine text="average" value={(props.good-props.bad)/(props.good+props.bad+props.neutral)} />
+        <StatisticLine text="positive" value={(100*props.good)/(props.good+props.bad+props.neutral)} symbol={"%"}/>
       </div>
     )
   }
+}
+
+const StatisticLine = (props) => {
+  return (
+    <div>
+      {props.text} {props.value} {props.symbol}
+    </div>
+  )
 }
 
 
