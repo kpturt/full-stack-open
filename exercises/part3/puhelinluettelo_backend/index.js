404e7ff146fdb3f4c706980eb065818887d59494
Kari Turtiainen <kari.p.turtiainen@utu.fi>
2021-10-18 22:57:24 +0300

    did 3.7

--- exercises/part3/puhelinluettelo_backend/index.js
+++ exercises/part3/puhelinluettelo_backend/index.js
@@ -2,8 +2,10 @@ console.log('>server starting...')
 
 const { response } = require('express')
 const express = require('express')
+var morgan = require('morgan')
 const app = express()
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
     {
